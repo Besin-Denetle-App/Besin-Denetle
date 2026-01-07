00304a7014c5d460cd83bf18f31f181719a1ab05
Furkan <66212252+Furkan-Pasa@users.noreply.github.com>
2026-01-07 21:44:55 +0300

    chore(shared): Shared Paketi Temizliği ve Sürüm Güncellemesi
    Shared kütüphanesindeki gereksiz derleme dosyaları temizlendi ve paket versiyonu güncellendi.

--- packages/shared/src/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type UUID = string;
-export type Timestamp = Date;
-export * from './barcode.types';
-export * from './product.types';
-export * from './user.types';
-export * from './vote.types';
