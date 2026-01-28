72bb0d61b9fe3237497d1c00696c4b4d308cad19
Furkan-Pasa <66212252+Furkan-Pasa@users.noreply.github.com>
2026-01-29 01:57:33 +0300

    Update

--- apps/backend/src/modules/scan/product/product.module.ts
+++ apps/backend/src/modules/scan/product/product.module.ts
@@ -4,6 +4,7 @@ import { Product } from '../../../entities';
 import { AiModule } from '../../ai/ai.module';
 import { VoteModule } from '../../vote/vote.module';
 import { BarcodeModule } from '../barcode/barcode.module';
+import { ContentModule } from '../content/content.module';
 import { ProductController } from './product.controller';
 import { ProductService } from './product.service';
 
@@ -16,6 +17,7 @@ import { ProductService } from './product.service';
     TypeOrmModule.forFeature([Product]),
     BarcodeModule,
     forwardRef(() => VoteModule),
+    forwardRef(() => ContentModule),
     AiModule,
   ],
   controllers: [ProductController],
